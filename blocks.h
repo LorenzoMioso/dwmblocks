cfe388fade9b5370a56e063f89a58381cf487168
LorenzoMioso <lorenzo.mioso@gmail.com>
2021-01-13 14:24:10 +0100

    automatic commit

--- blocks.h
+++ blocks.h
@@ -1,11 +1,15 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", "bar_update_datetime",					60,		0},
-	{"", "bar_update_wifi",					60,		0},
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+	{"", "bar_update_battery",			   60,		1},
+	{"", "bar_update_brightness",           0,		3},
+	{"", "bar_update_microphone",           0,		7},
+	{"", "bar_update_volume",               0,		11},
+	{"", "bar_update_bluetooth",            0,		2},
+	{"", "bar_update_wifi",                 0,		12},
+	{"", "bar_update_datetime",            60,		6},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = " | ";
+static char delim[] = " ] [ ";
 static unsigned int delimLen = 5;
