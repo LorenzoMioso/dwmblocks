c73fc86b72ed40a5d37addf0b50b7ef562632514
LorenzoMioso <lorenzo.mioso@gmail.com>
2022-06-12 17:09:47 +0200

    automatic commit

--- blocks.def.h
+++ blocks.def.h
@@ -3,14 +3,14 @@
 static const Block blocks[] = {
     /*Icon*/ /*Command*/ /*Update Interval*/ /*Update Signal*/
 
+    {"", "bar_update_datetime", 60, 6},
+    {"", "bar_update_capslock", 1, 14},
     {"", "bar_update_packages", 12 * 60 * 60, 8},
     {"", "bar_update_battery", 60, 1},
     {"", "bar_update_microphone", 0, 7},
     {"", "bar_update_volume", 0, 11},
     {"", "bar_update_bluetooth", 0, 2},
     {"", "bar_update_wifi", 60, 12},
-    {"", "bar_update_capslock", 1, 14},
-    {"", "bar_update_datetime", 60, 6},
 };
 
 // sets delimeter between status commands. NULL character ('\0') means no
