e43049b125289376431751f9b35799b9e86aac36
LorenzoMioso <lorenzo.mioso@gmail.com>
2022-06-20 19:45:41 +0200

    automatic commit

--- config.h
+++ config.h
@@ -4,9 +4,4 @@
 
 const Block blocks[] = {BLOCK("bar_update_datetime", 60, 8),
                         BLOCK("bar_update_packages", 24 * 60 * 60, 10),
-                        BLOCK("bar_update_battery", 60, 2),
-                        BLOCK("bar_update_microphone", 0, 9),
-                        BLOCK("bar_update_volume", 0, 13),
-                        BLOCK("bar_update_bluetooth", 60, 3),
-                        BLOCK("bar_update_capslock", 1, 5),
-                        BLOCK("bar_update_wifi", 60, 15)};
+                        BLOCK("bar_update_capslock", 1, 5)};
