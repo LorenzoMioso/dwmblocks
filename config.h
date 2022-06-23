3228858a8a561dfcb82bcb3359096076b0f46797
LorenzoMioso <lorenzo.mioso@gmail.com>
2022-06-24 00:07:18 +0200

    automatic commit

--- config.h
+++ config.h
@@ -2,6 +2,8 @@
 #define DELIMITER " "
 #define CLICKABLE_BLOCKS
 
-const Block blocks[] = {BLOCK("bar_update_datetime", 60, 8),
-                        BLOCK("bar_update_packages", 24 * 60 * 60, 10),
-                        BLOCK("bar_update_capslock", 1, 5)};
+const Block blocks[] = {
+    BLOCK("bar_update_packages", 24 * 60 * 60, 10),
+    BLOCK("bar_update_capslock", 0, 5),
+    BLOCK("bar_update_datetime", 60, 8),
+};
